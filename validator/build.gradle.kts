71b841f17974a8200711f35378b1ce47f32fc6b7
guy <guyskv@gmail.com>
2025-05-08 14:25:02 +0300

    Configure Maven publication for the release build variant of the validator module, including its AAR artifact, groupId, artifactId and version.

--- validator/build.gradle.kts
+++ validator/build.gradle.kts
@@ -37,3 +37,18 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = "com.github.guy-4444" // Change as needed
+                artifactId = "validator" // Change as needed
+                version = "1.00.04" // Change as needed
+                artifact(tasks.getByName("bundleReleaseAar"))
+
+                // Add dependencies to the Maven publication configuration (api or implementation)
+            }
+        }
+    }
+}
